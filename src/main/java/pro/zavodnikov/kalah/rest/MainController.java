bb5272443a2020520c72c6ecbf2defac4fe371b4
Dmitry Zavodnikov <d.zavodnikov@gmail.com>
2023-03-08 19:45:58 +0100

    Implement REST controllers

---
+++ src/main/java/pro/zavodnikov/kalah/rest/MainController.java
@@ -0,0 +1,59 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2023-2025 Dmitry Zavodnikov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package pro.zavodnikov.kalah.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+/**
+ * Spring Boot application.
+ *
+ * @author Dmitry Zavodnikov
+ */
+@RestController
+@Tag(name = "Main")
+@SpringBootApplication
+public class MainController extends AbstractController {
+
+    @GetMapping("/version")
+    public String getApiVersions() {
+        return API_V1;
+    }
+
+    @GetMapping("/")
+    @Hidden
+    public RedirectView staticContent() {
+        return new RedirectView("/static/index.html");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainController.class, args);
+    }
+}
